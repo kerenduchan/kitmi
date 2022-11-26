e9c2d02564852d299991703c1631de6ff6f716a7
Keren Duchan <keren.duchan@gmail.com>
2022-11-26 11:01:09 +0200

    specify autoincrement for int ids in db

--- db/schema.py
+++ db/schema.py
@@ -6,7 +6,7 @@ Base = sqlalchemy.ext.declarative.declarative_base()
 
 class Account(Base):
     __tablename__ = "accounts"
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     source = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     username = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -18,7 +18,7 @@ class Account(Base):
 
 class Category(Base):
     __tablename__ = "categories"
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
 
     def __repr__(self):
@@ -27,7 +27,7 @@ class Category(Base):
 
 class Subcategory(Base):
     __tablename__ = "subcategories"
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     category_id = sqlalchemy.Column(
         sqlalchemy.Integer, sqlalchemy.ForeignKey(Category.id), nullable=False)
@@ -38,7 +38,7 @@ class Subcategory(Base):
 
 class Payee(Base):
     __tablename__ = "payees"
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     subcategory_id = sqlalchemy.Column(
         sqlalchemy.Integer, sqlalchemy.ForeignKey(Subcategory.id), nullable=True)
