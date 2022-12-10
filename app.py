a20eb970c7db38010045b7e4df73469b2b72d4f0
Keren Duchan <keren.duchan@gmail.com>
2022-12-10 22:36:38 +0200

    allow kitmi-ui to access the graphql API

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import strawberry
 import fastapi
+from starlette.middleware.cors import CORSMiddleware
 import gql.query
 import gql.mutation
 import gql.context
-
 
 schema = strawberry.Schema(query=gql.query.Query, mutation=gql.mutation.Mutation)
 graphql_app = strawberry.fastapi.GraphQLRouter(schema, context_getter=gql.context.get_context)
 
 app = fastapi.FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+    max_age=3600,
+)
+
 app.include_router(graphql_app, prefix="/graphql")
