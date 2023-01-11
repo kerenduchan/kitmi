267d94b628473f5010cbf03598c0322340324baf
Keren Duchan <keren.duchan@gmail.com>
2023-01-11 06:54:14 +0200

    test_pagination using order by and limit (stateless)

--- test/test_pagination.py
+++ test/test_pagination.py
@@ -3,6 +3,8 @@ import sqlalchemy.ext.asyncio
 import sqlalchemy.ext.declarative
 import sqlalchemy.orm
 import random
+import typing
+import crypto
 
 Base = sqlalchemy.ext.declarative.declarative_base()
 
@@ -12,12 +14,80 @@ class Item(Base):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     data = sqlalchemy.Column(sqlalchemy.Integer)
 
+    def __repr__(self):
+        return f'{self.id}={self.data}'
 
-async def chunk_generator(async_result, chunk_size):
-    chunk = await async_result.fetchmany(chunk_size)
-    while len(chunk) > 0:
-        yield chunk
-        chunk = await async_result.fetchmany(chunk_size)
+
+class Chunk:
+    def __init__(self, recs: typing.List[Item], cursor_prev, cursor_next):
+        self.recs = recs
+        self.cursor_prev = cursor_prev
+        self.cursor_next = cursor_next
+
+    def __repr__(self):
+        recs_str = 'EMPTY'
+        if len(self.recs) > 0:
+            recs_str = f'{self.recs[0].id}-{self.recs[len(self.recs) - 1].id}'
+        s = f'{recs_str} ' \
+            f' prev=({self.get_decrypted_cursor(self.cursor_prev)}) ' \
+            f'next=({self.get_decrypted_cursor(self.cursor_next)})'
+        return s
+
+    @staticmethod
+    def get_decrypted_cursor(cursor):
+        if cursor is None:
+            return 'None'
+        elif cursor == '':
+            return ''
+        return crypto.Crypto().decrypt(cursor)
+
+
+async def get_next_chunk_of_items(session, chunk_size, cursor=''):
+
+    if cursor is None:
+        return Chunk([], None, None)
+
+    c = crypto.Crypto()
+    decrypted_cursor = 0
+    if cursor != '':
+        decrypted_cursor = int(c.decrypt(cursor))
+
+    # get one rec extra to determine whether there's a next chunk
+    sql = sqlalchemy.select(Item).where(Item.id > decrypted_cursor).order_by(Item.id).limit(chunk_size + 1)
+    recs = (await session.execute(sql)).scalars().all()
+
+    cursor_next = None
+    if len(recs) > chunk_size:
+        last_id = recs[chunk_size - 1].id
+        cursor_next = c.encrypt(str(last_id))
+        # remove the extra item from the end of the list
+        recs = recs[:-1]
+
+    chunk = Chunk(recs, cursor, cursor_next)
+    return chunk
+
+
+async def get_prev_chunk_of_items(session, chunk_size, cursor=''):
+    if cursor == '':
+        return Chunk([], None, '')
+
+    c = crypto.Crypto()
+    decrypted_cursor = 0
+    if cursor != '':
+        decrypted_cursor = int(c.decrypt(cursor))
+
+    sql = sqlalchemy.select(Item).where(Item.id <= decrypted_cursor).order_by(Item.id.desc()).limit(chunk_size + 1)
+    recs = (await session.execute(sql)).scalars().all()
+    recs.reverse()
+
+    cursor_prev = ''
+    if len(recs) > chunk_size:
+        first_id = recs[0].id
+        cursor_prev = c.encrypt(str(first_id))
+        # remove the extra item from the beginning of the list
+        recs = recs[1:]
+    c = Chunk(recs, cursor_prev, cursor)
+    return c
 
 
 async def test():
@@ -40,27 +110,30 @@ async def test():
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
-    # Fill a list with 10,007 values
-    data = [random.randint(1, 1000000) for i in range(10007)]
+    # Fill a list with values
+    data = [random.randint(1, 100) for i in range(101)]
 
     # Fill the 'items' table in the DB with these values
     async with session_maker() as session:
         session.add_all([Item(id=i, data=j) for i, j in enumerate(data, 1)])
         await session.commit()
 
-    # read the items from the DB chunk-by-chunk using the streaming API
-    async with engine.connect() as conn:
-        async_result = await conn.stream(sqlalchemy.select(Item))
+        chunk_size = 10
+        print(f'{chunk_size=}')
+        cursor = ''
 
-        chunk_size = 20
-        count = 0
-        chunks = chunk_generator(async_result, chunk_size)
-        async for chunk in chunks:
-            print(f"chunk: {chunk}")
-            count += len(chunk)
+        chunk = None
+        while cursor is not None:
+            chunk = await get_next_chunk_of_items(session, chunk_size, cursor)
+            print(chunk)
+            cursor = chunk.cursor_next
 
-        print(f'count={count}')
-        await async_result.close()
+        print('REACHED END. GOING BACK.')
+        cursor = chunk.cursor_prev
+        while cursor != '':
+            chunk = await get_prev_chunk_of_items(session, chunk_size, cursor)
+            print(chunk)
+            cursor = chunk.cursor_prev
 
     # Cleanup
     await engine.dispose()
